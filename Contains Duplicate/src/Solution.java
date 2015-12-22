21cfbc7c69a5ac77952216235ccbe150967760f7
hammertank <xiusuns@gmail.com>
2015-12-22 19:29:20 +0800

    201-12-21

---
+++ Contains Duplicate/src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+	public boolean containsDuplicate(int[] nums) {
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i : nums) {
+			if(!set.add(i)) {
+				return true;
+			} 
+		}
+		
+		return false;
+	}
+}
