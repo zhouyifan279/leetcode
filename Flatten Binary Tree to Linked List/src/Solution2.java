2cc791768a45ae96ef529891ee93a34fe4fc89a3
Moriarty <Moriarty@Moriarty-PC>
2015-12-25 19:50:55 +0800

    OMG

---
+++ Flatten Binary Tree to Linked List/src/Solution2.java
@@ -0,0 +1,14 @@
+
+public class Solution2 {
+	private TreeNode prev = null;
+
+	public void flatten(TreeNode root) {
+		if (root == null)
+			return;
+		flatten(root.right);
+		flatten(root.left);
+		root.right = prev;
+		root.left = null;
+		prev = root;
+	}
+}
